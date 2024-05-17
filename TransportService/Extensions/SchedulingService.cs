e5ef665b8b96feef709d2f35af21a2934067254d
Bardin08 <vbardin810@gmail.com>
2024-05-18 02:55:10 +0300

    Port TransportService

---
+++ TransportService/Extensions/SchedulingService.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Trumpee.MassTransit.Messages.Notifications;
+
+namespace TransportHub.Extensions;
+
+public static class SchedulingService
+{
+    public static async Task ProcessNotificationCommand<T>(
+        this ConsumeContext<T> ctx, Notification notification) where T : class
+    {
+        var expectedDeliveryTime = notification.Timestamp?.DateTime;
+        var priority = (byte)notification.Priority!;
+
+        var endpoint = new Uri($"queue:{notification.Recipient.Channel}");
+
+        var sendPipe = Pipe.Execute<SendContext<Notification>>(sendContext =>
+        {
+            sendContext.SetPriority(priority);
+        });
+
+        if (expectedDeliveryTime is null)
+        {
+            await ctx.Send(endpoint, notification, sendPipe);
+            return;
+        }
+
+        var isImmediate = expectedDeliveryTime - DateTime.UtcNow.AddSeconds(5) > TimeSpan.Zero;
+        var sendTask = isImmediate
+            ? ctx.ScheduleSend(endpoint, expectedDeliveryTime.Value, notification, sendPipe)
+            : ctx.Send(endpoint, notification, sendPipe);
+
+        await sendTask;
+    }
+}
--- TransportService/Program.cs
+++ TransportService/Program.cs
@@ -1,3 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using TransportHub;
+using Trumpee.MassTransit;
+using Trumpee.MassTransit.Configuration;
 
-Console.WriteLine("Hello, World!");
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("MassTransit", LogEventLevel.Debug)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var host = CreateHostBuilder(args).Build();
+await host.RunAsync();
+return;
+
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureHostConfiguration(config => config.AddEnvironmentVariables())
+        .UseSerilog()
+        .ConfigureServices((host, services) =>
+        {
+            var rabbitTopologyBuilder = new RabbitMqTransportConfigurator();
+            rabbitTopologyBuilder.AddExternalConfigurations(x =>
+            {
+                x.AddConsumer<CriticalNotificationsConsumer>();
+                x.AddConsumer<HighNotificationsConsumer>();
+                x.AddConsumer<MediumNotificationsConsumer>();
+                x.AddConsumer<LowNotificationsConsumer>();
+            });
+
+            rabbitTopologyBuilder.UseExternalConfigurations((ctx, cfg) =>
+            {
+                cfg.ReceiveEndpoint("delivery-critical", e =>
+                {
+                    e.BindQueue = true;
+                    e.PrefetchCount = 16;
+                    e.UseConcurrencyLimit(8);
+
+                    e.ConfigureConsumer<CriticalNotificationsConsumer>(ctx);
+                });
+
+                cfg.ReceiveEndpoint("delivery-high", e =>
+                {
+                    e.BindQueue = true;
+                    e.PrefetchCount = 12;
+                    e.UseConcurrencyLimit(6);
+
+                    e.ConfigureConsumer<HighNotificationsConsumer>(ctx);
+                });
+
+                cfg.ReceiveEndpoint("delivery-medium", e =>
+                {
+                    e.BindQueue = true;
+                    e.PrefetchCount = 8;
+                    e.UseConcurrencyLimit(4);
+
+                    e.ConfigureConsumer<MediumNotificationsConsumer>(ctx);
+                });
+
+                cfg.ReceiveEndpoint("delivery-low", e =>
+                {
+                    e.BindQueue = true;
+                    e.PrefetchCount = 4;
+                    e.UseConcurrencyLimit(2);
+
+                    e.ConfigureConsumer<LowNotificationsConsumer>(ctx);
+                });
+            });
+
+            services.AddConfiguredMassTransit(host.Configuration, rabbitTopologyBuilder);
+        });
+}
